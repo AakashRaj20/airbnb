6ad2607b45c855edfacb716d0f02288752196cca
aakashraj20 <aakashraj914@gmail.com>
2022-06-21 01:01:01 +0530

    Updates

---
+++ precache-manifest.b3eeb1e62df6c785e8b3ae395abf644e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e28f4b8ef7d705a27dcb",
+    "url": "/airbnb/static/css/main.200de60e.chunk.css"
+  },
+  {
+    "revision": "e28f4b8ef7d705a27dcb",
+    "url": "/airbnb/static/js/main.e28f4b8e.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/airbnb/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "3c4e28cc76839e97ed52",
+    "url": "/airbnb/static/js/2.3c4e28cc.chunk.js"
+  },
+  {
+    "revision": "31b594777ccc4fe16784",
+    "url": "/airbnb/static/js/runtime~main.31b59477.js"
+  },
+  {
+    "revision": "9a0291d416415d80b2062d23a6000c8e",
+    "url": "/airbnb/static/media/hero-img.9a0291d4.png"
+  },
+  {
+    "revision": "cdf7c634c5e467ad28bfbd94ff6e4947",
+    "url": "/airbnb/static/media/image.cdf7c634.png"
+  },
+  {
+    "revision": "5491ab6f454b1d295829977d59ff9091",
+    "url": "/airbnb/static/media/mountain.5491ab6f.png"
+  },
+  {
+    "revision": "ae17575579012e8dd502f15608dc3675",
+    "url": "/airbnb/static/media/wedding.ae175755.png"
+  },
+  {
+    "revision": "a2c9dc954fed8df6fa922413760bfb0b",
+    "url": "/airbnb/index.html"
+  }
+];
